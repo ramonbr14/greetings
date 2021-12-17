9dc25334b6136f50891eb8f2c75e523d78e6813d
ramonbr14 <ramonbr14@gmail.com>
2021-12-17 01:54:52 -0400

    Inicio do Assunto HATEOS, Aula 07 e 08

--- src/main/java/br/edu/ifam/greeting/GreetingApplication.java
+++ src/main/java/br/edu/ifam/greeting/GreetingApplication.java
@@ -2,7 +2,7 @@ package br.edu.ifam.greeting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//inicio aula 07 e 08
 @SpringBootApplication
 public class GreetingApplication {
 
